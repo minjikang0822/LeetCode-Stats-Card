813f024e83afb3244d7f83279e3bf4aee0155d81
Minji Kang <85323335+minjikang0822@users.noreply.github.com>
2023-05-23 11:47:09 +0900

    Update minji.ts

--- src/core/theme/minji.ts
+++ src/core/theme/minji.ts
@@ -2,7 +2,7 @@ import { Theme } from "./_theme";
 
 export default Theme({
     palette: {
-        bg: ["#101010", "#404040"],
+        bg: ["#ddd6f3", "#faaca8"],
         text: ["#f0f0f0", "#dcdcdc"],
         color: ["#ffa116", "#5cb85c", "#f0ad4e", "#d9534f"],
     },

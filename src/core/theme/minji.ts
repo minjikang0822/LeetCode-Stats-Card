056292143155db6bd61e8746b773c1605a181ffb
Minji Kang <85323335+minjikang0822@users.noreply.github.com>
2023-05-23 11:44:58 +0900

    Update minji.ts

--- src/core/theme/minji.ts
+++ src/core/theme/minji.ts
@@ -1,32 +1,10 @@
-import { Item } from "../item";
+import { Theme } from "./_theme";
 
-export interface Theme {
+export default Theme({
     palette: {
-        bg?: string[];
-        text?: string[];
-        color?: string[];
-    };
-    css: string;
-    extends?: Item;
-}
-
-export function Theme(theme: Partial<Theme>): Theme {
-    const completed: Theme = {
-        palette: {
-            bg: theme.palette?.bg ?? ["#fff", "#e5e5e5"],
-            text: theme.palette?.text ?? ["#000", "#808080"],
-            color: theme.palette?.color ?? [],
-        },
-        css: theme.css ?? "",
-        extends: theme.extends ?? undefined,
-    };
-
-    while (completed.palette.bg && completed.palette.bg.length < 4) {
-        completed.palette.bg.push(completed.palette.bg[completed.palette.bg.length - 1]);
-    }
-    while (completed.palette.text && completed.palette.text.length < 4) {
-        completed.palette.text.push(completed.palette.text[completed.palette.text.length - 1]);
-    }
-
-    return completed;
-}
+        bg: ["#101010", "#404040"],
+        text: ["#f0f0f0", "#dcdcdc"],
+        color: ["#ffa116", "#5cb85c", "#f0ad4e", "#d9534f"],
+    },
+    css: `#L{fill:#fff}`,
+});
